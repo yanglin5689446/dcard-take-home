3647ccafd769863ae1dc8441d2ea5f87bd483d19
Yang Lin <yanglin5689446@gmail.com>
2020-01-25 14:30:27 +0800

    Add basic search feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import Repo from './components/Repo'
 
-const App = () => (<div>Hello world</div>)
+const API_ENDPOINT = 'https://api.github.com/search/repositories'
+
+const App = () => {
+  const [input, setInput] = useState('')
+  const [repos, setRepos] = useState([])
+
+  const onSearch = useCallback(e => {
+    setInput(e.target.value)
+    fetch(`${API_ENDPOINT}?q=${e.target.value}`)
+      .then(response => response.json())
+      .then(json => {
+        setRepos(json.items)
+      })
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="p-4 row justify-content-center">
+        <div className="input-group col-12 col-md-6">
+          <input
+            className="form-control"
+            placeholder="Search Repo..."
+            type="text"
+            value={input}
+            onChange={onSearch}
+          />
+          <div className="input-group-append">
+            <button className="btn btn-outline-primary" type="button">
+              <i className="fa fa-search px-2" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="search-result p-2 p-md-5">
+        {
+          repos.map(repo => <Repo key={repo.id} {...repo} />)
+        }
+      </div>
+    </div>
+  )
+}
 
 export default App
---
+++ src/components/Repo/index.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+const Repo = ({ html_url, full_name, description, stargazers_count, language }) => (
+  <div className="repo card border-0">
+    <div className="card-body">
+      <h4 className="card-title">
+        <a href={html_url} target="_blank" rel="noopener noreferrer">{ full_name }</a>
+      </h4>
+      <p className="card-text">{ description }</p>
+      <div>
+        <span className="mr-2">
+          <i className="fas fa-star"/>
+          <span className="pl-2">{ stargazers_count }</span>
+        </span>
+        <span className="mr-2">
+          <i className="fas fa-star"/>
+          <span className="pl-2">{ language }</span>
+        </span>
+      </div>
+    </div>
+  </div>
+)
+
+export default Repo
